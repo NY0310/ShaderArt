9266e43b221b467e1a2180342b19d67702e300e9
NyPrivate <trident0310@gmail.com>
2019-10-12 23:35:03 +0900

    initcommit

---
+++ Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public Material mat;
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    /// <summary>
+    /// OnRenderImage is called after all rendering is complete to render image.
+    /// </summary>
+    /// <param name="src">The source RenderTexture.</param>
+    /// <param name="dest">The destination RenderTexture.</param>
+    void OnRenderImage (RenderTexture src, RenderTexture dest) {
+        Graphics.Blit (null, dest, mat);
+    }
+}
